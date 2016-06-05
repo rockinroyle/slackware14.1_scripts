866f0cb8d5248266ef181fdb415b7b4515a5621b
rockinroyle <rockinroyle@gmail.com>
2016-06-05 14:44:14 -0400

    Initial commit
     Changes to be committed:
    	new file:   chromium.sh

---
+++ chromium.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Chromium download script for slackware_14.1_x86_64.
+# Should be run from root!
+
+#
+# Variable declaration
+#
+
+TOP="/tmp"
+
+#
+# Cleanup
+#
+
+cd $TOP
+removepkg chromium-*.t?z
+cd $TOP/chromium
+rm -rf build
+cd $TOP/chromium-pepperflash
+rm -rf build
+cd $TOP/chromium-widevine
+rm -rf build
+
+#
+# Download
+#
+
+cd $TOP/chromium
+lftp -c "open http://www.slackware.com/~alien/slackbuilds/chromium/; mirror build"
+cd $TOP/chromium-pepperflash
+lftp -c "open http://www.slackware.com/~alien/slackbuilds/chromium-pepperflash-plugin/; mirror build"
+cd $TOP/chromium-widevine
+lftp -c "open http://www.slackware.com/~alien/slackbuilds/chromium-widevine-plugin/; mirror build"
+
+#
+# Build binary packages
+#
+
+# Chromium Browser
+cd $TOP/chromium/build
+sh ./chromium.SlackBuild
+
+# Pepperflash-plugin
+cd $TOP/chromium-pepperflash/build
+sh ./chromium-pepperflash-plugin.SlackBuild
+
+# Widevine-plugin
+cd $TOP/chromium-widevine/build
+sh ./chromium-widevine-plugin.SlackBuild
+
+#
+# Install compiled binary packages
+#
+
+cd $TOP
+installpkg chromium-*.t?z
+
+exit
