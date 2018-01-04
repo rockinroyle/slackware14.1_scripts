1ae8eba981c0e3be50f58067d5c42cbd68125212
rockinroyle <rockinroyle@gmail.com>
2018-01-04 17:10:45 -0500

    Get setup for fancy package updating script with function script.

---
+++ pkglibs.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Package library
+
+# Copyright (C) 2017-2018 @rockinroyle aka Ralph L Royle III
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/> 
+
+_chr () {
+	"wget http://bear.alienbase.nl/mirrors/people/alien/slackbuilds/chromium/pkg64/14.2/chromium-*.t?z"
+}
+
+_widiv () {
+	"wget http://bear.alienbase.nl/mirrors/people/alien/slackbuilds/chromium-widevine-plugin/pkg64/14.2/chromium-widevine-plugin-*.t?z"
+}
+
+_pepp () {
+	"wget http://bear.alienbase.nl/mirrors/people/alien/slackbuilds/chromium-pepperflash-plugin/pkg64/14.2/chromium-pepperflash-plugin-*.t?z"
+}
+
+_chrbr () {
+	"wget http://bear.alienbase.nl/mirrors/people/alien/slackbuilds/chromium/pkg64/14.2/chromium-*.t?z"
+	"wget http://bear.alienbase.nl/mirrors/people/alien/slackbuilds/chromium-widevine-plugin/pkg64/14.2/chromium-widevine-plugin-*.t?z"
+	"wget http://bear.alienbase.nl/mirrors/people/alien/slackbuilds/chromium-pepperflash-plugin/pkg64/14.2/chromium-pepperflash-plugin-*.t?z"
+}
